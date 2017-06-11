06583970e160ffe1dcd5d4f1e3cca0a2357fc456
Sáng Béo <Sng Bo>
2017-06-11 15:28:52 +0700

    Init admin

--- controllers/admin/pages_controller.php
+++ controllers/admin/pages_controller.php
@@ -1,6 +1,10 @@
 <?php
   class PagesController extends BaseController {
     function __construct() {
+      $this->check_login();
+      $this->check_admin();
+
+      $this->namespace = 'admin';
       $this->folder = 'pages';
     }
 
