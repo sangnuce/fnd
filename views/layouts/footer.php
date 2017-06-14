523a47f33149de42e3077bb4f74c2fc34b950e94
Sáng Béo <Sng Bo>
2017-06-14 19:16:27 +0700

    Static pages

---
+++ views/layouts/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer navbar-fixed-bottom">
+  <div class="container">
+    <span>Nhóm 2 - 58PM1 - Đại học Xây Dựng</span>
+    <div class="pull-right">Lan Anh - Thuỳ Dung - Sáng Béo</div>
+  </div>
+</footer>
